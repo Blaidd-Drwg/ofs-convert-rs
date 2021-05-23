7b4909e344c16d800102bb91cd89109928aa5b72
Lukas Behrendt <lukas.behrendt@student.hpi.de>
2021-05-23 20:18:13 +0200

    Unify indentation

--- src/lohi.rs
+++ src/lohi.rs
@@ -10,105 +10,105 @@ use num::PrimInt;
 /// to read and write these values without having to care about their on-disk representation.
 pub struct LoHiMut<'a, Full, Half>
 where Full: PrimInt + From<Half>,
-	  Half: PrimInt + TryFrom<Full>,
-	  Half::Error: Debug {
-	pub lo: &'a mut Half,
-	pub hi: &'a mut Half,
-	full: PhantomData<Full>,
+      Half: PrimInt + TryFrom<Full>,
+      Half::Error: Debug {
+    pub lo: &'a mut Half,
+    pub hi: &'a mut Half,
+    full: PhantomData<Full>,
 }
 
 impl<'a, Full, Half> LoHiMut<'a, Full, Half>
 where Full: PrimInt + From<Half>,
-	  Half: PrimInt + TryFrom<Full>,
-	  Half::Error: Debug {
+      Half: PrimInt + TryFrom<Full>,
+      Half::Error: Debug {
 
-	const HALF_BIT_COUNT: usize = std::mem::size_of::<Half>() * 8;
+    const HALF_BIT_COUNT: usize = std::mem::size_of::<Half>() * 8;
 
-	// ideally would be const, but zero is not a const fn
-	fn lower_half_mask() -> Full {
-		(!Half::zero()).into()
-	}
+    // ideally would be const, but zero is not a const fn
+    fn lower_half_mask() -> Full {
+        (!Half::zero()).into()
+    }
 
-	pub fn new(lo: &'a mut Half, hi: &'a mut Half) -> Self {
-		assert_eq!(2 * std::mem::size_of::<Half>(), std::mem::size_of::<Full>(), "Attempting to create a LoHi where `Half` is not half the size of `Full`.");
-		Self { lo, hi, full: PhantomData }
-	}
+    pub fn new(lo: &'a mut Half, hi: &'a mut Half) -> Self {
+        assert_eq!(2 * std::mem::size_of::<Half>(), std::mem::size_of::<Full>(), "Attempting to create a LoHi where `Half` is not half the size of `Full`.");
+        Self { lo, hi, full: PhantomData }
+    }
 
-	pub fn get(&self) -> Full {
-		let hi: Full = (*self.hi).into();
-		let lo: Full = (*self.lo).into();
-		(hi << Self::HALF_BIT_COUNT) + lo
-	}
+    pub fn get(&self) -> Full {
+        let hi: Full = (*self.hi).into();
+        let lo: Full = (*self.lo).into();
+        (hi << Self::HALF_BIT_COUNT) + lo
+    }
 
-	pub fn set(&mut self, value: Full) {
-		*self.lo = Half::try_from(value & Self::lower_half_mask()).unwrap();
-		*self.hi = Half::try_from(value >> Self::HALF_BIT_COUNT).unwrap();
-	}
+    pub fn set(&mut self, value: Full) {
+        *self.lo = Half::try_from(value & Self::lower_half_mask()).unwrap();
+        *self.hi = Half::try_from(value >> Self::HALF_BIT_COUNT).unwrap();
+    }
 }
 
 impl<'a, Full, Half> AddAssign<Full> for LoHiMut<'a, Full, Half>
 where Full: PrimInt + From<Half>,
-	  Half: PrimInt + TryFrom<Full>,
-	  Half::Error: Debug {
-	fn add_assign(&mut self, other: Full) {
-		self.set(self.get() + other);
-	}
+      Half: PrimInt + TryFrom<Full>,
+      Half::Error: Debug {
+    fn add_assign(&mut self, other: Full) {
+        self.set(self.get() + other);
+    }
 }
 
 impl<'a, Full, Half> SubAssign<Full> for LoHiMut<'a, Full, Half>
 where Full: PrimInt + From<Half>,
-	  Half: PrimInt + TryFrom<Full>,
-	  Half::Error: Debug {
-	fn sub_assign(&mut self, other: Full) {
-		self.set(self.get() - other);
-	}
+      Half: PrimInt + TryFrom<Full>,
+      Half::Error: Debug {
+    fn sub_assign(&mut self, other: Full) {
+        self.set(self.get() - other);
+    }
 }
 
 
 
 pub struct LoHi<'a, Full, Half>
 where Full: PrimInt + From<Half>,
-	  Half: PrimInt + TryFrom<Full>,
-	  Half::Error: Debug {
-	pub lo: &'a Half,
-	pub hi: &'a Half,
-	full: PhantomData<Full>,
+      Half: PrimInt + TryFrom<Full>,
+      Half::Error: Debug {
+    pub lo: &'a Half,
+    pub hi: &'a Half,
+    full: PhantomData<Full>,
 }
 
 impl<'a, Full, Half> LoHi<'a, Full, Half>
 where Full: PrimInt + From<Half>,
-	  Half: PrimInt + TryFrom<Full>,
-	  Half::Error: Debug {
+      Half: PrimInt + TryFrom<Full>,
+      Half::Error: Debug {
 
-	const HALF_BIT_COUNT: usize = std::mem::size_of::<Half>() * 8;
+    const HALF_BIT_COUNT: usize = std::mem::size_of::<Half>() * 8;
 
-	pub fn new(lo: &'a Half, hi: &'a Half) -> Self {
-		Self { lo, hi, full: PhantomData }
-	}
+    pub fn new(lo: &'a Half, hi: &'a Half) -> Self {
+        Self { lo, hi, full: PhantomData }
+    }
 
-	pub fn get(&self) -> Full {
-		let hi: Full = (*self.hi).into();
-		let lo: Full = (*self.lo).into();
-		(hi << Self::HALF_BIT_COUNT) + lo
-	}
+    pub fn get(&self) -> Full {
+        let hi: Full = (*self.hi).into();
+        let lo: Full = (*self.lo).into();
+        (hi << Self::HALF_BIT_COUNT) + lo
+    }
 }
 
 impl<'a, Full, Half> Add<Full> for LoHi<'a, Full, Half>
 where Full: PrimInt + From<Half>,
-	  Half: PrimInt + TryFrom<Full>,
-	  Half::Error: Debug {
-	type Output = Full;
-	fn add(self, other: Full) -> Self::Output {
-		self.get() + other
-	}
+      Half: PrimInt + TryFrom<Full>,
+      Half::Error: Debug {
+    type Output = Full;
+    fn add(self, other: Full) -> Self::Output {
+        self.get() + other
+    }
 }
 
 impl<'a, Full, Half> Sub<Full> for LoHi<'a, Full, Half>
 where Full: PrimInt + From<Half>,
-	  Half: PrimInt + TryFrom<Full>,
-	  Half::Error: Debug {
-	type Output = Full;
-	fn sub(self, other: Full) -> Self::Output {
-		self.get() - other
-	}
+      Half: PrimInt + TryFrom<Full>,
+      Half::Error: Debug {
+    type Output = Full;
+    fn sub(self, other: Full) -> Self::Output {
+        self.get() - other
+    }
 }
--- src/util.rs
+++ src/util.rs
@@ -1,23 +1,23 @@
 /// Extension trait for a convenience method which transmutes a slice to a slice of another type 
 /// while ensuring correct alignment and size.
 pub trait ExactAlign {
-	/// SAFETY: See the documentation for `slice::align_to`
-	unsafe fn exact_align_to<'a, Target>(&'a self) -> &'a [Target];
+    /// SAFETY: See the documentation for `slice::align_to`
+    unsafe fn exact_align_to<'a, Target>(&'a self) -> &'a [Target];
 }
 
 impl<T> ExactAlign for [T] {
-	unsafe fn exact_align_to<'a, Target>(&'a self) -> &'a [Target] {
-		let (before, target, after) = self.align_to::<Target>();
-		assert!(before.is_empty());
-		assert!(after.is_empty());
-		target
-	}
+    unsafe fn exact_align_to<'a, Target>(&'a self) -> &'a [Target] {
+        let (before, target, after) = self.align_to::<Target>();
+        assert!(before.is_empty());
+        assert!(after.is_empty());
+        target
+    }
 }
 
 /// Stopgap function to convert an ASCII string from a FAT short name into a null-terminated UTF-16 string like
 /// from a FAT long name
 pub fn short_name_to_long_name(short_name: &str) -> Vec<u16> {
-	let mut name: Vec<_> = short_name.chars().map(|c| c as u16).collect();
-	name.push(0);
-	name
+    let mut name: Vec<_> = short_name.chars().map(|c| c as u16).collect();
+    name.push(0);
+    name
 }
