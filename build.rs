6b7f1bc6bc2f29117b34946954bdd08ea39de128
Lukas Behrendt <lukas.behrendt@student.hpi.de>
2021-09-30 17:39:38 +0200

    Remove unnecessary build script and C files

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let dst = cmake::Config::new("ofs-convert").build_target("ofs-convert").build();
-
-    println!("cargo:rustc-link-search={}/build", dst.display());
-
-    println!("cargo:rustc-link-lib=static=ofs-convert");
-    println!("cargo:rustc-link-lib=static=uuid");
-    println!("cargo:rustc-link-lib=static=stdc++");
-}
--- ofs-convert/visualizer_types.h
+++
@@ -1,7 +0,0 @@
-ENTRY(FAT, "Teal")
-ENTRY(BlockGroupHeader, "SkyBlue")
-ENTRY(OriginalPayload, "PaleGreen")
-ENTRY(ResettledPayload, "BurlyWood")
-ENTRY(Ext4Dir, "PaleVioletRed")
-ENTRY(IdxNode, "Indigo")
-ENTRY(StreamArchiverPage, "Black")  // TODO pretty color
