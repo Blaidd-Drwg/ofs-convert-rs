d2d8405320c69d33ca9e0ebb3d4abf8d7f19de7a
Lukas Behrendt <lukas.behrendt@student.hpi.de>
2021-11-02 18:00:01 +0100

    Run the entire test suite in the Docker container

--- test/container/main.sh
+++ test/container/main.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-CARGO_TARGET_DIR=/target
+export CARGO_TARGET_DIR=/target
 rsync -r /dependencies/target/ "$CARGO_TARGET_DIR"
 cd /project_root
-cargo build --target-dir "$CARGO_TARGET_DIR"
+cargo build || exit 1
+cargo test --no-run || exit 1
+printf "\n\n######################## UNIT TESTS #########################\n\n"
+cargo test
+printf "\n\n##################### UNIT TESTS (SUDO) #####################\n\n"
+cargo test-sudo
+printf "\n\n##################### INTEGRATION TESTS #####################\n\n"
 cd /test
-python3 run.py /target/debug/ofs-convert-rs tests
+python3 run.py "$CARGO_TARGET_DIR/debug/ofs-convert-rs" tests
