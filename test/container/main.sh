d9d96d564fba395b3dd2e8c76130d62aa3b005c4
Lukas Behrendt <lukas.behrendt@student.hpi.de>
2021-11-03 15:53:40 +0100

    Use simpler and more efficient way to cache dependencies in Docker image

--- test/container/main.sh
+++ test/container/main.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+# To avoid showing warnings multiple times, we redirect stderr to /dev/null every time we run `cargo` except when we build the tests
 export CARGO_TARGET_DIR=/target
-rsync -r /dependencies/target/ "$CARGO_TARGET_DIR"
 cd /project_root
-cargo build || exit 1
+printf     "########################### BUILD ###########################\n\n"
+cargo build 2>/dev/null|| exit 1
 cargo test --no-run || exit 1
 printf "\n\n######################## UNIT TESTS #########################\n\n"
-cargo test
+cargo test 2>/dev/null
 printf "\n\n##################### UNIT TESTS (SUDO) #####################\n\n"
-cargo test-sudo
+cargo test-sudo 2>/dev/null
 printf "\n\n##################### INTEGRATION TESTS #####################\n\n"
 cd /test
 python3 run.py "$CARGO_TARGET_DIR/debug/ofs-convert-rs" tests
--- test/container/run.sh
+++ test/container/run.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 PROJECT_ROOT=$(realpath "$(dirname "$0")/../..")
 TEST_DIR=$PROJECT_ROOT/test
-TARGET_DIR=$TEST_DIR/container/target
-
-mkdir -p "$TARGET_DIR"
 docker run --rm \
            --privileged \
            -v "$PROJECT_ROOT":/project_root:ro \
            -v "$TEST_DIR":/test \
-           -v "$TARGET_DIR":/target \
            -e OFS_CONVERT_TOOL_TIMEOUT="$OFS_CONVERT_TOOL_TIMEOUT" \
            ofs-convert-testing
